039dde8cad2526de0448900abcdd3299c96f3245
Lakshya Mahajan <68889369+coolcrab28@users.noreply.github.com>
2024-06-12 01:21:50 +0530

    latest commit

--- neurons_np/neuron.py
+++ neurons_np/neuron.py
@@ -4,5 +4,5 @@ weights = [[0.2, 0.8, -0.5, 1],
 [0.5, -0.91, 0.26, -0.5],
 [-0.26, -0.27, 0.17, 0.87]]
 biases = [2.0, 3.0, 0.5]
-layer_outputs = np.dot(weights, inputs) + biases
+layer_outputs = np.dot(inputs, np.transpose(weights)) + biases
 print(layer_outputs)
